b99517974dcdc8240dfd2bf21d8fc98906840961
GB-Titi <tristanmatteoli@gmail.com>
2021-12-03 10:10:15 +0100

    Ajout call api + call infos pokemons fonctionnel sur le promise all

--- src/hooks/useApi/index.tsx
+++ src/hooks/useApi/index.tsx
@@ -2,23 +2,33 @@ import { resolve } from "dns";
 import React, { useEffect, useState } from "react";
 import { Pokedex } from "../../pages";
 
+type Pokemon = {}
+ 
+type Pokedex = {
+    count: number;
+    next: string;
+    previous: string;
+    results : {name: string; url: string}[]
+}
+
 const useApi = () => {
-    const [pokedex, setPokedex] = useState([]);
-    const [pokemon, setInfo] = useState([]);
+    const [pokedex, setPokedex] = useState<Pokedex|null>(null);
+    const [pokemon, setInfo] = useState<any>([]);
 
     useEffect(() => {
         fetch("https://pokeapi.co/api/v2/pokemon/")
             .then(res => res.json())
-            .then(setPokedex)
+            .then((res: Pokedex) => setPokedex(res))
     }, [])
 
     useEffect(() => {
-        if (pokedex.length > 0) {
+        if (pokedex) {
             Promise.all(
-                pokedex.map((pokemon: any) => new Promise(resolve => fetch(pokemon.url).then((info) => resolve({ pokemon, info }))))
+                pokedex.results.map((pokemon, i) => new Promise(resolve => setTimeout(() => 
+                fetch(pokemon.url).then(res => res.json()).then((info) => resolve({ pokemon, info })), 50 * i)))
             ).then((pokemonsInfo: any) => {
                 const newArr = pokemon.concat(pokemonsInfo);
-                console.log("array: " + newArr);
+                console.log("🚀 ~ file: index.tsx ~ line 31 ~ ).then ~ newArr", newArr)
                 setInfo(newArr);
             })
         }
