14e2ec62ab6d2c5ffccd4f645e7d51e523bc8af6
GB-Titi <tristanmatteoli@gmail.com>
2022-01-22 15:25:47 +0100

    Mise à jour affichage liste, pokedex etc...

--- src/pages/Pokedex/index.tsx
+++ src/pages/Pokedex/index.tsx
@@ -4,17 +4,50 @@ import "./index.css";
 import { useApi } from "../../hooks";
 
 const Pokedex = () => {
-    const { pokemon } = useApi();
-    useEffect(() => {
-      if(pokemon.length > 0){
-        // console.log("🚀 ~ file: index.tsx ~ line 10 ~ useEffect ~ pokemon", pokemon.map((pokemons: any) => ({
-        //     types: pokemons.info.types.map((t: any) => t.type.name)
-        // })))
-        console.log("test:", pokemon.map((pokemons: any) =>pokemons.info.types.map((t: any/*, i: number*/) => t.type.name)))
-        console.log("description: ",pokemon.map((pokemons: any) =>pokemons.info.species.url))
-      }
-    }, [pokemon])
+    const { pokemon, setInfo } = useApi();
+    // useEffect(() => {
+    //   if(pokemon.length > 0){
+    //     // console.log("🚀 ~ file: index.tsx ~ line 10 ~ useEffect ~ pokemon", pokemon.map((pokemons: any) => ({
+    //     //     types: pokemons.info.types.map((t: any) => t.type.name)
+    //     // })))
+    //     // console.log(pokemon)
+
+    //     // Promise.all(
+    //     //     pokedex.results.map((pokemon, i) => new Promise(resolve =>  setTimeout(() =>
+    //     //     fetch(pokemon.url).then(res => res.json()).then((info) => resolve({ pokemon, info })), 15 * i)))
+    //     // ).then((pokemonsInfo: any) => {
+    //     //     const newArr = pokemon.concat(pokemonsInfo);
+    //     //     console.log("🚀 ~ file: index.tsx ~ line 31 ~ ).then ~ newArr", newArr)
+    //     //     setInfo(newArr);
+    //     // })
+
+    //     // new Promise(resolve => fetch(pokemon.info.species.url).then((description) => resolve(setInfo(description))))
+    //     // // console.log("test:", pokemon.map((pokemons: any) =>pokemons.info.types.map((t: any/*, i: number*/) => t.type.name)))
+    //     // // console.log("description: ",pokemon.map((pokemons: any) =>fetch(pokemons.info.species.url).then(res => res.json())))
+    //     console.log('TRY DESCRIPTION :' + pokemon)
+    //   }
+    // }, [pokemon])
+    
     // console.log(pokemon.map((pokemons: any) => console.log(pokemons.info.abilities)));
+
+    if (!pokemon) {
+        return (
+          <IonPage>
+            <IonHeader class="ion-justify-content-start">
+                <IonToolbar>
+                    <IonButtons slot="start">
+                        <IonBackButton defaultHref="/" />
+                        <IonTitle>Pokédex</IonTitle>
+                    </IonButtons>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent>
+              <p>Chargement</p>
+            </IonContent>
+          </IonPage>
+        );
+      }
+
     return (
         <IonPage id="PokedexPage">
             <IonHeader class="ion-justify-content-start">
